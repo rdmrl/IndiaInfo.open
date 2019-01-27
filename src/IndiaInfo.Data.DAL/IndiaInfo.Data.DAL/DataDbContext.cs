3bd10e390534e3815f3f5a6d5ba5e713299726dc
Ravi Damarla <ravi@endofthenight.com>
2019-01-28 00:19:45 +0530

    initial commit

---
+++ src/IndiaInfo.Data.DAL/IndiaInfo.Data.DAL/DataDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndiaInfo.Data.DAL
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions options) : base(options) { }
+    }
+}
